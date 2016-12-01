5a50d0ccee0f0cdbb07b7a68bcfa6cf08ec7c512
Bingham, Brian S <bsbingha@nps.edu>
2016-12-01 13:48:44 -0800

    increment

--- pypid/scratch.py
+++ pypid/scratch.py
@@ -24,3 +24,25 @@ val = tmp
 for attr in msgAttrList:
     val = getattr(val,attr)
 print val
+
+
+import functools
+
+def rsetattr(obj, attr, val):
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+#sentinel = object()
+def rgetattr(obj, attr, default=sentinel):
+    if default is sentinel:
+        _getattr = getattr
+    else:
+        def _getattr(obj, name):
+            return getattr(obj, name, default)
+    return functools.reduce(_getattr, [obj]+attr.split('.'))
+
+
+rsetattr(tmp,'linear.x',2.0)
+print tmp.linear.x
+x = rgetattr(tmp,'linear.x')
+print x
